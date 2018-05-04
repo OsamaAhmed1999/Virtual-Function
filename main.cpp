8eff7761f0dcc1ea21a689051462a69c7cf5dce4
Osama Ahmed <osamaahmed6198@gmail.com>
2018-05-04 23:01:02 +0500

    Dynamic Object Inheritance

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-  AdjTableLamp Lamp;
-  cout << Lamp << endl;
-  Lamp.print(cout);
-  Lamp.dim();
-  Lamp.dim();
-  Lamp.dim();
-  Lamp.print(cout);
-  Lamp.press_switch();
-  Lamp.print(cout);
+  AdjTableLamp *Lamp = new AdjTableLamp();
+  cout << *Lamp << endl;
+  Lamp->print(cout);
+  Lamp->dim();
+  Lamp->dim();
+  Lamp->dim();
+  Lamp->print(cout);
+  Lamp->press_switch();
+  Lamp->print(cout);
 
   system("pause");
 }
